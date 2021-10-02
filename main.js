338b89b2cf49ccf0d5111bd0732ad7c5cc52768e
valwebsite <74025716+valwebsite@users.noreply.github.com>
2021-10-01 22:00:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+camera = document.getElementById("camera");
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+Webcam.attach('#camera');
+
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log("ml5 version:", ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/aMGPvDos-/model.json', modelLoaded);
+function modelLoaded(){
+    console.log('Model loaded')
+}
